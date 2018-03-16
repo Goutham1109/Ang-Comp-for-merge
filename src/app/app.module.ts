0af1d0874b62e25490ec7de5343fcd9ed4f5e085
Goutham S <goutham.s@cloudnowtech.com>
2018-03-16 14:54:12 +0530

    Angular files for merge with java

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { BikeInfoComponent } from './bike-info/bike-info.component';
+import { BikeService } from './bike.service';
+import { HttpModule } from '@angular/http';
+import{AppRoutingModule} from './app-routing/app-routing.module';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {   MatListItem } from '@angular/material'
+import { MatInputModule } from '@angular/material';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikesComponent,
+    BikeInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+      MatListModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatButtonModule
   ],
-  providers: [],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
